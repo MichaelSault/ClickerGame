a38298fdbf21a67e37486799622a5c8b3585e474
Michael Sault <msaul025@uottawa.ca>
2020-01-03 23:57:42 -0500

    pirate update 1
    Done:
    -changed theme to pirate
    -added additional upgrades
    -changed how upgrade prices are determined
    
    To Do:
    -limit number of pirates to be dependent on number of ships
    -change icons to match the pirate theme
    -further tweak cost of upgrades vs. dps
    -css/layout changes

--- clicker.js
+++ clicker.js
@@ -1,60 +1,86 @@
-var cookiecount = 0;
-var autoClick = 0;
-var miners = 0;
-var cps = 0;
+var balance = 0;
+var Scallywags = 0;
+var pirates = 0;
+var firstmates = 0;
+var captains = 0;
+var dps = 0;
 var manualClick = 1;
 
 function update() {
-    document.getElementById('text').value = cookiecount;
-    document.getElementById('cps').value = cps;
+    document.getElementById('doubloons').value = balance;
+    document.getElementById('dps').value = dps;
 
-    document.title = cookiecount + " Cookies";
-    document.getElementById('ammountAutoClick').innerHTML = "You own " + autoClick + " Auto Clickers";
-    document.getElementById('costAutoClick').innerHTML = ((autoClick+1)*12) + " Cookies";
-    document.getElementById('ammountMiners').innerHTML = "You own " + miners + " Miners";
-    document.getElementById('costMiners').innerHTML = ((miners+1)*36) + " Cookies";
+    document.title = balance + " Doubloons";
+    document.getElementById('ammountScallywags').innerHTML = "You own " + Scallywags + " Scallywags";
+    document.getElementById('costScallywags').innerHTML = (5*(Scallywags*Scallywags+1)) + " Doubloons";
+    document.getElementById('ammountPirates').innerHTML = "You own " + pirates + " Pirates";
+    document.getElementById('costPirates').innerHTML = (250*(pirates*pirates+1)) + " Doubloons";
+    document.getElementById('ammountFMates').innerHTML = "You own " + firstmates + " First Mates";
+    document.getElementById('costFMates').innerHTML = (1000*(firstmates*firstmates+1)) + " Doubloons";
+    document.getElementById('ammountCaptains').innerHTML = "You own " + captains + " First Mates";
+    document.getElementById('costCaptains').innerHTML = (50000*(captains*captains+1)) + " Doubloons";
 }
 
 function timer(){
-    cookiecount = cookiecount + autoClick + (miners*5);
-    cps = autoClick + (miners*5);
-    manualClick = 1+Math.round(cps*0.1);
+    balance = balance + Scallywags + (pirates*5) + (firstmates*25) + (captains*125);
+    dps = Scallywags + (pirates*5) + (firstmates*25) + (captains*125);
+    manualClick = 1+Math.round(dps*0.1);
     update();
 }
 setInterval(timer, 1000);
 
 function add() {
-    cookiecount = cookiecount + manualClick;
+    balance = balance + manualClick;
     update();
 }
 
 function save() {
-    localStorage.setItem("cookiecount", cookiecount);
-    localStorage.setItem("autoClick", autoClick);
-    localStorage.setItem("miners", miners);
+    localStorage.setItem("balance", balance);
+    localStorage.setItem("Scallywags", Scallywags);
+    localStorage.setItem("pirates", pirates);
+    localStorage.setItem("firstmates", firstmates);
+    localStorage.setItem("captains", captains);
 }
 
 function load() {
-    cookiecount = localStorage.getItem("cookiecount");
-    cookiecount = parseInt(cookiecount);
-    autoClick = localStorage.getItem("autoClick");
-    autoClick = parseInt(autoClick);
-    miners = localStorage.getItem("miners");
-    miners = parseInt(miners);
+    balance = localStorage.getItem("balance");
+    balance = parseInt(balance);
+    Scallywags = localStorage.getItem("Scallywags");
+    Scallywags = parseInt(Scallywags);
+    pirates = localStorage.getItem("pirates");
+    pirates = parseInt(pirates);
+    firstmates = localStorage.getItem("firstmates");
+    firstmates = parseInt(firstmates);
+    captains = localStorage.getItem("captains");
+    captains = parseInt(captains);
     update();
 }
 
-function buyAutoClick() {
-    if (cookiecount >= ((autoClick+1)*12)) {
-        cookiecount = cookiecount - ((autoClick+1)*12);
-        autoClick = autoClick + 1;
+function buyScallywags() {
+    if (balance >= (5*(Scallywags*Scallywags+1))) {
+        balance = balance - (5*(Scallywags*Scallywags+1));
+        Scallywags = Scallywags + 1;
         update();
     }
 }
-function buyMiner() {
-    if(cookiecount >= (miners+1)*36){
-        cookiecount = cookiecount - ((miners+1)*36);
-        miners = miners + 1;
+function buyPirate() {
+    if(balance >= (250*(pirates*pirates+1))){
+        balance = balance - (250*(pirates*pirates+1));
+        pirates = pirates + 1;
+        update();
+    }
+}
+function buyFMate() {
+    if(balance >= (1000*(firstmates*firstmates+1))){
+        balance = balance - (1000*(firstmates*firstmates+1));
+        firstmates = firstmates + 1;
+        update();
+    }
+}
+function buyCaptain() {
+    if(balance >= (1000*(captains*captains+1))){
+        balance = balance - (1000*(captains*captains+1));
+        captains = captains + 1;
         update();
     }
 }
